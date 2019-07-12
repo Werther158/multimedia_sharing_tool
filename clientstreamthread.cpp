a48b7d1484b56c37ac29b160cf41e4ff01630164
Werther <werther.berselli@gmail.com>
2019-07-12 21:15:22 +0200

    Fix in localhost connections on Nvidia Jetson Nano

--- clientstreamthread.cpp
+++ clientstreamthread.cpp
@@ -17,9 +17,13 @@ void ClientStreamThread::run()
 {
     emit sendStartStreamingCommand();
     std::string command;
-    //std::system("bash -c \"killall ibus-daemon\"");
-    command = "bash -c \"ffplay -window_title 'MST Streaming' -rtsp_flags listen rtsp://" +
-            Configurations::server_ip + "@" + Configurations::my_own_used_ip + ":" + std::to_string(Configurations::port + 1) + "\"";
+    std::system("bash -c \"killall ibus-daemon\"");
+    if(Configurations::server_ip != Configurations::my_own_used_ip)
+        command = "bash -c \"ffplay -window_title 'MST Streaming' -rtsp_flags listen rtsp://" +
+                Configurations::server_ip + "@" + Configurations::my_own_used_ip + ":" + std::to_string(Configurations::port + 1) + "\"";
+    else
+        command = "bash -c \"ffplay -window_title 'MST Streaming' -rtsp_flags listen rtsp://" +
+                Configurations::server_ip + ":" + std::to_string(Configurations::port + 1) + "\"";
     std::system(command.c_str());
     emit streamingEnded();
 }
--- serverstreamthread.cpp
+++ serverstreamthread.cpp
@@ -18,8 +18,12 @@ void ServerStreamThread::run()
     std::string command;
     // Sleep 3 seconds to allow Client to start listening correctly
     sleep(3);
-    command = "bash -c \"ffmpeg -re -y -i " + Configurations::file_name + " -codec copy -f rtsp -rtsp_transport tcp rtsp://" +
-            Configurations::my_own_used_ip + "@" + Configurations::client_ip + ":" + std::to_string(Configurations::port + 1) + "\"";
+    if(Configurations::client_ip != Configurations::my_own_used_ip)
+        command = "bash -c \"ffmpeg -re -y -i " + Configurations::file_name + " -codec copy -f rtsp -rtsp_transport tcp rtsp://" +
+                Configurations::my_own_used_ip + "@" + Configurations::client_ip + ":" + std::to_string(Configurations::port + 1) + "\"";
+    else
+        command = "bash -c \"ffmpeg -re -y -i " + Configurations::file_name + " -codec copy -f rtsp -rtsp_transport tcp rtsp://" +
+                Configurations::client_ip + ":" + std::to_string(Configurations::port + 1) + "\"";
     std::system(command.c_str());
     if(!streaming_ended)
         emit stopStream();
